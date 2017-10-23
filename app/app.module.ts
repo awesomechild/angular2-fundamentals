758f54b94f2fe312c07256715a7795c39338cf68
Shanur Rehman <rahman.shanur7@gmail.com>
2017-10-24 00:04:35 +0530

    Wired up all the files manually
    will be using angular-cli for furthur applictions

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+// this is the module that does the bootstrapping of the first element
+// module has a decorator of @NgModule
+
+
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { EventsAppComponent} from './events-app.component'
+
+@NgModule({
+    imports:[BrowserModule],
+    //components have to registered in the modules therefore we do this
+    declarations:[EventsAppComponent],
+    //now bootstrap the EventsAppComponent here
+    bootstrap: [EventsAppComponent]
+    // create a ts file now that we can load from our index.html that will do our initial bootstrapping and load this module
+})
+
+export class AppModule{
+
+}
+
+//you load the ts file from the index.html which inturn loads the module which is this, this will inturn bootstrap all the 
+// components and everything else
---
+++ app/events-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { } from ''
+
+
+@Component({
+    selector: "events-app",                 
+    // how to call this component => selector can be thought of as a name for component
+    template: "<h2>Hello World</h2>"
+    // what it renders is the template
+})
+export class EventsAppComponent{
+
+}
