f4c56183aadc426b0362f759f5523b3ba1abd763
Shanur Rehman <rahman.shanur7@gmail.com>
2017-10-25 01:19:57 +0530

    Adding styles

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -7,10 +7,7 @@ import { Component } from '@angular/core'
     <div>
         <h1>Upcoming Angular 2 Events</h1>
         <hr/>
-        <event-thumbnail (eventClick)="handleEventClicked($event)" [event]="event1"></event-thumbnail>
-         <!-- whenever you receive an input event (eventClick) from other component fire event handleEventClicked($event) in your 
-         component $(event) is the data emitted along with out event, you can only pass a single value with event so if you want 
-         to pass multiple values with event wrap them into an object -->
+        <event-thumbnail [event]="event1"></event-thumbnail>
     </div>
     `
 })
@@ -28,8 +25,5 @@ export class EventsListComponent{
             state:'Arunchal Pradesh',
             country:'India'
         }
-    }
-    handleEventClicked(data){
-        console.log('received: ', data.name);
     }
 }
