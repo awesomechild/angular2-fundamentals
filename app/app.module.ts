4f06a355698028a2062338268126796d13980d41
Shanur Rehman <rahman.shanur7@gmail.com>
2017-10-26 01:01:31 +0530

    Implemented a toastr service
    Created a new class TaostrService which mocks toastr
    so that unit testing can be applied to it later on

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,7 +9,7 @@ import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { EventService } from "./events/shared/event.service";
-
+import { ToastrService} from "./common/toastr.service"
 
 @NgModule({
     imports:[BrowserModule],
@@ -20,7 +20,7 @@ import { EventService } from "./events/shared/event.service";
         NavBarComponent
     ],
     bootstrap: [EventsAppComponent],
-    providers:[EventService]
+    providers:[EventService, ToastrService]
 })
 
 export class AppModule{
