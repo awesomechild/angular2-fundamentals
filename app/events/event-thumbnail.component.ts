90fbc1f1df200db3b704a9e0abbbec64939ba452
Shanur Rehman <rahman.shanur7@gmail.com>
2017-10-25 06:47:31 +0530

    Diifferent methods of styling components
    here i have described three different ways of styling components using ngClass --

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -7,8 +7,8 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
         <div class="well hoverwell thumbnail">
             <h2>{{event?.name}}</h2> 
             <div>Date:{{event?.date}}</div>
-            <div [class.green]="event.time === '8:00 am'" [ngSwitch]="event?.time">
-            <!--if the expression event.time === '8:00 am' returns true then add the class green to this div element is what it is saying -->
+            <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+                        
             Time:{{event?.time}}
                 <span *ngSwitchCase="'8:00 am'">(Early start)</span>
                 <span *ngSwitchCase="'10:00 am'">(Late start)</span>
@@ -38,4 +38,20 @@ export class EventThumbnailComponent{
     // will not show the value of the key
     @Input() event:any
 
+    getStartTimeClass(){
+        // METHOD 1 of returning objects to ngClass
+        // const isEarlyStart = this.event && this.event.time === '8:00 am'
+        // return {green: isEarlyStart, bold: isEarlyStart}
+
+        // METHOD 2 using string literal, return the names of the classes you would like to add seperated by spaces
+        // if(this.event && this.event.time === '8:00 am')
+        //     return 'green bold'
+        // return ''
+        // you will have to set the return type of this method to ensure typescripts type safety 
+
+        // METHOD3 using arrays
+        if(this.event && this.event.time === '8:00 am')
+            return ['green', 'bold']
+        return []
+    }
 }
