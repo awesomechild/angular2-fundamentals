e9fa1a345fbf3fc5877e7fcfd7acabcd5b6ce4a0
Shanur Rehman <rahman.shanur7@gmail.com>
2017-10-29 10:48:48 +0530

    Validating reactive forms - 1
    right now i have some logic in the template - like validating information, next i am going to pull that logic into component so that it is reusable, i will add the same styles again but by calling a function

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core'
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: 'app/user/profile.component.html'
+  templateUrl: 'app/user/profile.component.html',
+  styles:[`
+    em { float:right; color#E05C65; padding-left:10px;}
+    .error input { background-color :#E3C3C5; }
+    .error :: -webkit-input-placeholder { color: #999;}
+    .error :: -moz-placeholder { color: #999;}
+    .error : -moz-placeholder { color: #999;}
+    .error :ms-input-placeholder { color: #999;}
+  `
+  ]
 })
 export class ProfileComponent implements OnInit{
   profileForm:FormGroup
@@ -15,8 +24,14 @@ export class ProfileComponent implements OnInit{
     ngOnInit () {
       // first value in the form control is the value you would like pre populated
       // in the field => that will come from the authService so inject it
-      let firstName = new FormControl(this.authService.currentUser.firstName)
-      let lastName = new FormControl(this.authService.currentUser.lastName)
+      
+      // One of the benefits of using reactive forms is that we can define our code
+      // in the component where it is unit testable.
+      // Validators are just passed in as the second parameters of the form FormControl
+      // so we can just use Validators.required as the second parameter
+
+      let firstName = new FormControl(this.authService.currentUser.firstName, Validators.required)
+      let lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
 
       this.profileForm = new FormGroup({
         firstName:firstName,
@@ -26,9 +41,12 @@ export class ProfileComponent implements OnInit{
     cancel(){
       this.router.navigate(['/events'])
     }
-    saveProfile(formValues){
-      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-      this.router.navigate(['events'])
-    }
 
+    // lets add the logic that the form is saved only if it is validated
+    saveProfile(formValues){
+      if (this.profileForm.valid){
+        this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+        this.router.navigate(['events'])
+      }
+    }
 }
