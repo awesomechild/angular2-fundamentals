1d98089888c5230bf47b2b863e71203ea47fc96b
Shanur Rehman <rahman.shanur7@gmail.com>
2017-11-07 15:05:29 +0530

    Custom Validator
    Added a custom validator in reactive form exercise

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
-
+import { restrictedWords } from "../shared/index";
 @Component({
-    templateUrl: 'app/events/event-details/create-session.component.html'
+    templateUrl: 'app/events/event-details/create-session.component.html',
+    styles: [`
+    em { float:right; color:#E05C65; padding-left:10px;}
+    .error input, .error input, .error textarea { background-color :#E3C3C5; }
+    .error :: -webkit-input-placeholder { color: #999;}
+    .error :: -moz-placeholder { color: #999;}
+    .error : -moz-placeholder { color: #999;}
+    .error :ms-input-placeholder { color: #999;}
+  `]
 })
 
 export class CreateSessionComponent implements OnInit{
@@ -12,14 +20,13 @@ export class CreateSessionComponent implements OnInit{
     duration:FormControl
     level:FormControl
     abstract:FormControl
-
     ngOnInit(): void {
         this.name = new FormControl('', Validators.required)
         this.presenter = new FormControl('', Validators.required)
         this.duration = new FormControl('', Validators.required)
         this.level = new FormControl('', Validators.required)
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)])
-
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])])
+ 
 
         this.newSessionForm = new FormGroup({
             name:this.name,
@@ -29,7 +36,17 @@ export class CreateSessionComponent implements OnInit{
             abstract:this.abstract
         })
     }
+
     saveSession(formValues){
-        console.log(formValues);
+        let session: any = {
+            id: undefined,
+            name: formValues.name,
+            duration: +formValues.duration,
+            level: formValues.level,
+            presenter: formValues.presenter,
+            abstract: formValues.abstract,
+            voters: []
+        }
+        console.log(session)
     }
 }
