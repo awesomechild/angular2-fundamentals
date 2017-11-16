dc77f7fbb7b602e325ce85fd32dbeca862edff01
Shanur Rehman <rahman.shanur7@gmail.com>
2017-11-16 10:20:47 +0530

    Opaque token to register dependencies
    Registering dependencies instead of globally defining them is very important for performance(Tree shaking and stuff)

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Inject } from '@angular/core'
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { TOASTR_TOKEN, Toastr } from '../common/toastr.service';
+
 
 @Component({
   templateUrl: 'app/user/profile.component.html',
@@ -15,23 +17,15 @@ import { Router } from '@angular/router';
   `
   ]
 })
+
 export class ProfileComponent implements OnInit{
   profileForm:FormGroup
-  //since firstName and last name are both of type form control we would like to pull them up of the ngOnInit so we do this
     private firstName: FormControl
     private lastName: FormControl
-    constructor(private authService: AuthService, private router:Router){
+    constructor(private authService: AuthService, private router:Router, @Inject(TOASTR_TOKEN) private toastr: Toastr){
       
     }
     ngOnInit () {
-      // first value in the form control is the value you would like pre populated
-      // in the field => that will come from the authService so inject it
-      
-      // One of the benefits of using reactive forms is that we can define our code
-      // in the component where it is unit testable.
-      // Validators are just passed in as the second parameters of the form FormControl
-      // so we can just use Validators.required as the second parameter
-
       this.firstName = new FormControl(this.authService.currentUser.firstName, Validators.required)
       this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
 
@@ -44,15 +38,12 @@ export class ProfileComponent implements OnInit{
       this.router.navigate(['/events'])
     }
 
-    // lets add the logic that the form is saved only if it is validated
     saveProfile(formValues){
       if (this.profileForm.valid){
         this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-        this.router.navigate(['events'])
+        this.toastr.success('Profile Saved')
       }
     }
-    //logic to be added into the function
-    // {'error':profileForm.controls.lastName.invalid && profileForm.controls.lastName.touched }
     validateFirstName(){
       return this.firstName.valid || this.firstName.untouched
     }
